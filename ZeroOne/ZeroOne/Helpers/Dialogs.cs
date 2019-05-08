47d746bdead31e401738d188eac6b125c73e1f25
Олег Кравчук <oleg680014445@gmail.com>
2019-05-08 14:53:12 +0300

    OpenFileDialog

---
+++ ZeroOne/ZeroOne/Helpers/Dialogs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ZeroOne.Helpers
+{
+    class Dialogs
+    {
+        static string[] OpenFileDialog(System.Windows.Forms.IWin32Window owner = null)
+        {
+            using (var open = new System.Windows.Forms.OpenFileDialog())
+            {
+                open.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                open.Filter = 
+                    "Текстовые документы (*.txt)|*.txt|" +
+                    "Portable Document Format (*.pdf)|*.pdf|" +
+                    "Rich Text Format (*.rtf)|*.rtf|";
+                open.FilterIndex = 1;
+                open.FileName = "";
+                open.Multiselect = true;                             
+                open.CheckPathExists = true;
+                open.CheckFileExists = true;
+
+                open.ShowDialog(owner);
+
+                return open.FileNames;
+            }
+        }
+
+
+
+    }
+}
