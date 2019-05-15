ab26bc2484d071918649790018794ec9034d17e7
Mamoru <32417036+Drift2020@users.noreply.github.com>
2019-05-15 14:27:24 +0300

    Revert "Merge branch 'master' into Oleg"
    This reverts commit 06db81a1797b5699502bb9b093dc21b978d75e42, reversing
    changes made to b6902f008132ea759b8992d08238aaa1f8536f8b.

---
+++ ZeroOne/ZeroOne/Readers/TxtReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using ZeroOne.Documents;
+using ZeroOne.Interfaces;
+
+namespace ZeroOne.Readers
+{
+    class TxtReader : IReader
+    {
+        public async Task<IDocument> Read(string path)
+        {
+            string buf = string.Empty;
+
+            using (var fStream = new StreamReader(path, Encoding.Default))
+                buf = await fStream.ReadToEndAsync();
+                
+
+            return new TxtDocument(buf);
+        }
+    }
+}
