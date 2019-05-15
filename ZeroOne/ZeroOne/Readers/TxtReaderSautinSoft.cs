1adbb979872ffaaea9256696e24e7dfe2385392e
Mamoru <32417036+Drift2020@users.noreply.github.com>
2019-05-15 14:27:47 +0300

    Revert "Update TxtReaderSautinSoft.cs"
    This reverts commit b6902f008132ea759b8992d08238aaa1f8536f8b.

--- ZeroOne/ZeroOne/Readers/TxtReaderSautinSoft.cs
+++ ZeroOne/ZeroOne/Readers/TxtReaderSautinSoft.cs
@@ -14,7 +14,7 @@ namespace ZeroOne.Readers
     {
         public Task<IDocument> Read(string path)
         {
-            //return Task.Run(async () => { });
+            //return Task.Factory.StartNew(() => {  });
 
             //DocumentCore txt = DocumentCore.Load(path, new TxtLoadOptions());
             return null;
